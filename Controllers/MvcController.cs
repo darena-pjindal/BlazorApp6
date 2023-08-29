b27fbe6e2b41c880e1f5f74ae6dcc5552fe060c2
Pawan Jindal <45796558+darena-pjindal@users.noreply.github.com>
2023-08-29 11:52:07 -0500

    Initial commit

---
+++ Controllers/MvcController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApp6.Controllers
+{
+    public class MvcController : Controller
+    {
+
+        [Route("mvc")]
+        public IActionResult Index()
+        {
+            return Redirect("https://www.microsoft.com");
+            
+        }
+    }
+}
